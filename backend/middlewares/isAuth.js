918ed97a78c9dd34e730175a1641fb09eb9aa0c3
sonuchaurasiya7633 <sonukumar763303@gmail.com>
2025-10-03 21:50:39 +0530

    food

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 const isAuth = async (req, res, next) => {
   try {
     const token = req.cookies.token;
+    console.log("All Token here =",token)
     if (!token) {
       return res.status(400).json({ message: "Token not Found" });
     }
--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-const genToken =async (userId) =>{
+const genToken = async (userId) =>{
   try {
     const token = await jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"})
     return token;
