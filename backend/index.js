b6c5fb4d853a9af3a0cb312836c028bab1918dd9
sonuchaurasiya7633 <sonukumar763303@gmail.com>
2025-10-03 12:52:58 +0530

    food app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();  
-import { connect } from 'mongoose';
+
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
@@ -22,7 +22,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-    origin:"http://localhost:5173",
+    origin:"https://campus-food-sonu-hfgk.onrender.com",
     credentials:true,
     methods:['POST','GET'] 
 }
@@ -32,7 +32,7 @@ app.set("io",io)
 
 const port = process.env.PORT || 5000;
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://campus-food-sonu-hfgk.onrender.com",
     credentials:true
 }))
 
