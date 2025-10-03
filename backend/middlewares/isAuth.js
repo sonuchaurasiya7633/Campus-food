f9f19143ee02b6e0339f9fbcef3bea0b07035489
Sonu Kumar <sonukumar763303@gmail.com>
2025-10-03 22:00:33 +0530

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -3,9 +3,9 @@ const isAuth = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     console.log("All Token here =",token)
-    if (!token) {
-      return res.status(400).json({ message: "Token not Found" });
-    }
+    // if (!token) {
+    //   return res.status(400).json({ message: "Token not Found" });
+    // }
     const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
     if (!decodeToken) {
       return res.status(400).json({ message: "Token not verify" });
